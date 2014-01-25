1ff6c6335d20cb07720344495c76e5ea4518b22d
narudemi <senpaisense@gmail.com>
2014-01-25 17:58:29 +0700

    versi 1.2

--- main.cpp
+++ main.cpp
@@ -9,13 +9,11 @@ using namespace std;
 
 const int tinggi = 9;
 const int lebar = 9;
-const char Wall = '#';
-const char Free = ' ';
-const char SomeDude = '*';
+const char batas = '#';
+const char kosong = ' ';
+const char langkah = '*';
 
-//printf("Masukkan maksimal lebar = ");
-//scanf("%i", tinggi);
-char Maze[tinggi][lebar + 1] =
+char labirin[tinggi][lebar + 1] =
 {
     "# #######",
     "#       #",
@@ -25,82 +23,72 @@ char Maze[tinggi][lebar + 1] =
     "#     # #",
     "# ### # #",
     "#       #",
-    "####### #",
+    "##### ###",
 };
 
 class koordinat
 {public:
-    int X;
-    int Y;
-    koordinat(int x = 0, int y = 0) { X = x, Y = y; }
-    koordinat(const koordinat &koordinat) { X = koordinat.X; Y = koordinat.Y; }
+    int I;
+    int J;
+    koordinat(int i=0, int j=0){ I = i, J = j; }
+    koordinat(const koordinat &koordinat) {I = koordinat.I; J=koordinat.J;}
 };
 
 koordinat StartingPoint(1, 0);
-koordinat EndingPoint(7, 8);
+koordinat EndingPoint(5, 8);
 
 void cetak()
 {
-    for (int Y = 0; Y < tinggi; Y++)
+    for (int J = 0; J < tinggi; J++)
     {
         textcolor(YELLOW);
-        cout<<Maze[Y]<<endl;
+        cout<<labirin[J]<<endl;
     }
     cout<<endl;
 }
 
-bool Solve(int X, int Y)
+bool solusi(int I, int J)
 {
-    // Make the move (if it's wrong, we will backtrack later.
-    Maze[Y][X] = SomeDude;
+    //membuat langkah
+    labirin[J][I] = langkah;
 
-    // If you want progressive update, uncomment these lines...
+    //mencetak langkah backtracking
     cetak();
-    //Sleep(50);
-
-    // Check if we have reached our goal.
-    if (X == EndingPoint.X && Y == EndingPoint.Y)
+   
+    if (I == EndingPoint.I && J == EndingPoint.J)
     {
         return true;
     }
-
-    // Recursively search for our goal.
-    if (X > 0 && Maze[Y][X - 1] == Free && Solve(X - 1, Y))
+    //Pencarian secara rekursif
+    if (I > 0 && labirin[J][I - 1] == kosong && solusi(I - 1, J))
     {
         return true;
     }
-    if (X < lebar && Maze[Y][X + 1] == Free && Solve(X + 1, Y))
+    if (I < lebar && labirin[J][I + 1] == kosong && solusi(I + 1, J))
     {
         return true;
     }
-    if (Y > 0 && Maze[Y - 1][X] == Free && Solve(X, Y - 1))
+    if (J > 0 && labirin[J - 1][I] == kosong && solusi(I, J - 1))
     {
         return true;
     }
-    if (Y < tinggi && Maze[Y + 1][X] == Free && Solve(X, Y + 1))
+    if (J < tinggi && labirin[J + 1][I] == kosong && solusi(I, J + 1))
     {
         return true;
     }
-
-    // Otherwise we need to backtrack and find another solution.
-    Maze[Y][X] = Free;
-
-    // If you want progressive update, uncomment these lines...
+    //untuk backtracking
+    labirin[J][I] = kosong;
+    //mencetak langkah backtracking
     cetak();
-    //Sleep(50);
     return false;
 }
 
 int main(int argc)
 {
-    
-    if (Solve(StartingPoint.X, StartingPoint.Y))
+    if (solusi(StartingPoint.I, StartingPoint.J))
     {
         cetak();
-    }
-    else
-    {
-        printf("Tidak ada solusi\n");
+    }else{ cout<<"Tidak ada solusi"<<endl;
     }
 	getch();
     return 0;
